6b2ac38288b5d4c02a770465e6c58dd342f9afd1
Jim <jimdong@damaiapp.com.tw>
2025-09-08 11:46:35 +0800

    9

--- apps/web/tests/e2e/helpers.ts
+++
@@ -1,8 +0,0 @@
-import type { Page } from '@playwright/test'
-
-export async function e2eLogin(page: Page, email = 'z887700101703027@gmail.com') {
-  await page.addInitScript((token) => {
-    localStorage.setItem('E2E_BEARER', token as string)
-  }, `Bearer e2e:${email}`)
-}
-
